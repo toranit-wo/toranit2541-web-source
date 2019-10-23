a23dd3591c2216e39754af841d34c679d72714a2
duenchay <duenchay.wo.60@ubu.ac.th>
2019-10-24 01:39:43 +0700

    ooo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Website
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.2.
 
 ## Development server
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UsersComponent } from './users/users.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'card', component: CardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'users', component: UsersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

c728c7c10a32c2bb724a2fc8d9319a1a63061a17
duenchay <duenchay.wo.60@ubu.ac.th>
2019-10-24 02:17:57 +0700

    777

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,4 +1,5 @@
 export const  firebaseConfig = {
+
   apiKey: "AIzaSyB-MhJdR2fLCoN76-YXaloDAjYcnjjXHXI",
   authDomain: "toppic-6cecb.firebaseapp.com",
   databaseURL: "https://toppic-6cecb.firebaseio.com",
@@ -7,5 +8,5 @@ export const  firebaseConfig = {
   messagingSenderId: "652777279267",
   appId: "1:652777279267:web:d6db05292a04f351e70696",
   measurementId: "G-G4QQ0C8R0X"
-
+   
   };
